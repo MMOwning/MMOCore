f0b7c831b2b07c2a514de7ea934af7cc77528152
Exitare <exi@pihaar.de>
2015-07-23 16:00:38 +0200

    [SQL] Insert Emerald Dream

---
+++ sql/mmo_updates_world/world_emerald_dream.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES ('169', '0', '0', '0');
+
+REPLACE INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item_level`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `quest_failed_text`, `comment`) VALUES ('169', '3', '80', '80', '0', '0', '0', '0', '0', '0', '0', 'Emerald Forest');
