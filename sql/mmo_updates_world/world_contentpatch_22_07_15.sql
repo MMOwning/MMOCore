540dfab3ec85cbb37033034233190aa8afe7685f
Exitare <exi@pihaar.de>
2015-07-23 19:23:06 +0200

    [SQL] Update some NPC´s

--- sql/mmo_updates_world/world_contentpatch_22_07_15.sql
+++ sql/mmo_updates_world/world_contentpatch_22_07_15.sql
@@ -882,6 +882,8 @@ REPLACE INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`eq
 REPLACE INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`) VALUES 
 ('60006','60001','0','1','1','18718','0','-12957.91','-1777.30','136.65','3.27','25','0','0','1000000','250000','0');
 /* Bei 60008 weitermachen */
+REPLACE INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`) VALUES 
+('60008','60001','530','1','1','18718','0','-2791.75','487.38','-24.59','0.48','25','0','0','1000000','250000','0');
 
 
 REPLACE INTO `npc_text` (ID, text0_0, text0_1, BroadcastTextID0, lang0, Probability0, em0_0, em0_1, em0_2, em0_3, em0_4, em0_5, text1_0, text1_1, BroadcastTextID1, lang1, Probability1, em1_0, em1_1, em1_2, em1_3, em1_4, em1_5, text2_0, text2_1, BroadcastTextID2, lang2, Probability2, em2_0, em2_1, em2_2, em2_3, em2_4, em2_5, text3_0, text3_1, BroadcastTextID3, lang3, Probability3, em3_0, em3_1, em3_2, em3_3, em3_4, em3_5, text4_0, text4_1, BroadcastTextID4, lang4, Probability4, em4_0, em4_1, em4_2, em4_3, em4_4, em4_5, text5_0, text5_1, BroadcastTextID5, lang5, Probability5, em5_0, em5_1, em5_2, em5_3, em5_4, em5_5, text6_0, text6_1, BroadcastTextID6, lang6, Probability6, em6_0, em6_1, em6_2, em6_3, em6_4, em6_5, text7_0, text7_1, BroadcastTextID7, lang7, Probability7, em7_0, em7_1, em7_2, em7_3, em7_4, em7_5, VerifiedBuild) VALUES
@@ -893,6 +895,7 @@ REPLACE INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`
 REPLACE INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `OptionBroadcastTextID`, `option_id`, `npc_option_npcflag`, `box_text`) VALUES ('60001', '4', '0', 'Boss: Teleport zu Orrig', '0', '1', '1', 'Soll geportet werden?');
 REPLACE INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `OptionBroadcastTextID`, `option_id`, `npc_option_npcflag`, `box_text`) VALUES ('60001', '5', '0', 'Boss: Teleport Exitares Schatten', '0', '1', '1', 'Soll geportet werden?');
 REPLACE INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `OptionBroadcastTextID`, `option_id`, `npc_option_npcflag`, `box_text`) VALUES ('60001', '6', '0', 'Boss: Teleport Gequälte Seele', '0', '1', '1', 'Soll geportet werden?');
+REPLACE INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `OptionBroadcastTextID`, `option_id`, `npc_option_npcflag`, `box_text`) VALUES ('60001', '7', '0', 'Boss: Kayoula', '0', '1', '1', 'Soll geportet werden?');
 
 
 SET @ENTRY := 60001;
@@ -1081,6 +1084,35 @@ REPLACE INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_typ
 /* target_o */3.5,
 /* comment  */"Teleport Geqälte Seele");
 
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+REPLACE INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,
+/*  id */  7,
+/* link  */0,
+/* event_type*/62,
+/* event_phase_mask */0,
+/* event_chance  */100,
+/* event_flags  */0,
+/* event_param1 */60001,
+/* event_param2  */7,
+/* event_param3   */0,
+/* event_param4 */0,
+/* action_type  */62,
+/* action_param1 */530,
+/* action_param2 */0,
+/* action_param3  */0,
+/* action_param4  */0,
+/* action_param5  */0,
+/* action_param6 */0,
+/* target_type  */7,
+/* target_param1 */0,
+/* target_param2*/0,
+/* target_param3  */0,
+/* target_x  */-2793.37,
+/* target_y   */493.29,
+/* target_z  */-25.33,
+/* target_o */0.42,
+/* comment  */"Teleport_Kayoula");
 
 
 
