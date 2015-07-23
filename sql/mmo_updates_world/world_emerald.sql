4aab01a1f79ce42dd50757bdcaf0540961e8dbe1
Exitare <exi@pihaar.de>
2015-07-23 16:40:43 +0200

    [SQL] Merge old Emerald Dream to new one

--- sql/mmo_updates_world/world_emerald.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES ('169', '0', 'emerald_dream', '0');
-INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES ('1601', '-366.091', '3097.86', '92.317', '0.0487625', '169', 'EmeraldDream');
