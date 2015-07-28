98106f8da28f4699c0fd4b9e382ef7d6847ff60b
Exitare <exi@pihaar.de>
2015-07-28 17:26:29 +0200

    [SQL] Add new Vendor

--- sql/mmo_updates_world/world_eventnpc.sql
+++ sql/mmo_updates_world/world_eventnpc.sql
@@ -1,3 +1,5 @@
+DELETE FROM `creature_template` WHERE `entry` = 60003;
+
 REPLACE INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`,`faction` ,`npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `BaseAttackTime`, `RangeAttackTime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) 
 VALUES ( /*  ID */ 60003,
   /*  Dificculty 1 */0,
@@ -68,6 +70,7 @@ VALUES ( /*  ID */ 60003,
   /*  flags_extra` */0, 
   /* Scriptname  */'');
   
+DELETE FROM `creature` WHERE `id` = 60003;   
   
 REPLACE INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`spawndist`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`) VALUES 
 ('500997','60003','0','1','1','17072','0','-8791.39','640.36','94.46','3.77','25','0','0','1000000','250000','0');
