b5bc340310f3e3bb471ab8cf8786c61296a9ab1d
Exitare <raphael.kirchgaessner@gmail.com>
2015-09-24 16:24:10 +0200

    [SQL] LIGHT HARDMODE ANPASSUNG

--- sql/mmo_updates_world/world_quest_anpassungen.sql
+++ sql/mmo_updates_world/world_quest_anpassungen.sql
@@ -2,4 +2,11 @@ UPDATE `quest_template` SET `RequiredNpcOrGo2`='0', `RequiredNpcOrGoCount2`='0' 
 REPLACE INTO `creature_questender` (`id`, `quest`) VALUES ('800027', '900800');
 REPLACE INTO `creature_questender` (`id`, `quest`) VALUES ('800027', '900801');
 
+/* LIGHT HARDMODE LOOTANPASSUNG */
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('800063', '46964', '0', '25', '0', '4', '5', '1', '1');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('800063', '46991', '0', '25', '0', '4', '5', '1', '1');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('800063', '46993', '0', '25', '0', '4', '5', '1', '1');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `Reference`, `Chance`, `QuestRequired`, `LootMode`, `GroupId`, `MinCount`, `MaxCount`) VALUES ('800063', '46986', '0', '25', '0', '4', '5', '1', '1');
+UPDATE `creature_loot_template` SET `LootMode`='5' WHERE `Entry`='800063' and`Item`='50300';
 
+--Ende
