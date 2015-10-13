c2bb4752b416302de48a333024d3aea38978a195
Exitare <raphael.kirchgaessner@gmail.com>
2015-10-13 18:37:49 +0200

    [Custom] firstnpc

--- src/server/scripts/Custom/firstnpc.cpp
+++ src/server/scripts/Custom/firstnpc.cpp
@@ -93,7 +93,8 @@ class npc_first_char : public CreatureScript
 								 pPlayer->GetGUID();
 								 ChatHandler(pPlayer->GetSession()).PSendSysMessage("[Aufwertungs System] Deine Aufwertung wurde ausgefuehrt. Viel Spass wuenscht Exitare sowie das MMOwning-Team.",
 									 pPlayer->GetName());
-								sWorld->SendGMText(LANG_GM_BROADCAST, "Eine Firstausstattung wurde ausgefuehrt");
+								 uint32 id = pPlayer->GetSession()->GetAccountId();
+								sWorld->SendGMText(LANG_GM_BROADCAST, "Eine Firstausstattung wurde ausgefuehrt von dem Spieler mit der Accountid: %u",id);
 								pPlayer->PlayerTalkClass->SendCloseGossip();
 								pPlayer->SetLevel(80);
 								pPlayer->TeleportTo(0, -795.73, 1495.50, 104.54, 1.05, 0);
@@ -243,7 +244,6 @@ class npc_first_char : public CreatureScript
 									pPlayer->SetMoney(50000000);
 									
 									uint32 id = pPlayer->GetSession()->GetAccountId();
-									
 									sWorld->SendGMText(LANG_GM_BROADCAST, "Eine 10er Gildenausstattung wurde ausgefuehrt vom Spieler mit der Accountid: %u", id);
 									CharacterDatabase.PExecute("REPLACE INTO first_char "
 										"(guid,Charname, account, Accname, time, guildid,ip) "
@@ -334,7 +334,8 @@ class npc_first_char : public CreatureScript
 								pPlayer->TeleportTo(0, -795.73, 1495.50, 104.54, 1.05, 0);
 								pPlayer->AddItem(20400, 4);
 								pPlayer->SetMoney(50000000);
-								sWorld->SendGMText(LANG_GM_BROADCAST, "Eine 25er Gildenausstattung wurde ausgefuehrt");
+								uint32 id = pPlayer->GetSession()->GetAccountId();
+								sWorld->SendGMText(LANG_GM_BROADCAST, "Eine 25er Gildenausstattung wurde ausgefuehrt von dem Spieler mit der Accountid: %u",id);
 								CharacterDatabase.PExecute("REPLACE INTO first_char "
 									"(guid,Charname, account, Accname, time, guildid,ip) "
 									"VALUES ('%u', '%s', %u, '%s', %u, %u, '%s')",
