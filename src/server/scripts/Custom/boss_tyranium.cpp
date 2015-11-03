7cc2e547626f6de129d71a7f59fca0d2bb0ea061
Exitare <raphael.kirchgaessner@gmail.com>
2015-11-03 11:42:29 +0100

    [Custom] tyranium

--- src/server/scripts/Custom/boss_tyranium.cpp
+++ src/server/scripts/Custom/boss_tyranium.cpp
@@ -145,9 +145,10 @@ public:
 			
 			if (spell->Id == 45462){
 				armor = me->GetArmor();
+				Player* target;
 				me->SetName("Eonar der Alte");
 				me->SetObjectScale(2);
-				me->Whisper("Eure Seuchen. Bitte fuehrt sie weiter aus. Dient mir als Sklave wenn ich Euch unterwerfe.", LANG_UNIVERSAL,caster,false);
+				me->Whisper("Eure Seuchen. Bitte fuehrt sie weiter aus. Dient mir als Sklave wenn ich Euch unterwerfe.", LANG_UNIVERSAL, target, false);
 				me->CombatStop(true);
 				armor = armor + 10;
 				me->SetArmor(armor);
