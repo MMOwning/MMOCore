9015017c925d1a3c567db82de5624231d083e3d2
Exitare <exi@pihaar.de>
2015-11-23 16:01:22 +0100

    [Custom] codenpc

--- src/server/scripts/Custom/ex_codenpc.cpp
+++ src/server/scripts/Custom/ex_codenpc.cpp
@@ -54,49 +54,68 @@ public: codenpc() : CreatureScript("codenpc"){ }
 
 		bool OnGossipSelectCode(Player* player, GameObject* /*go*/, uint32 /*sender*/, uint32 action, const char* code){
 		
-			uint32 spieler = player->GetGUID();	
-			std::string codes = code;
-
-			if (!*code){
-				char msg[250];
-				snprintf(msg, 250, "Es wurde kein Code eingetragen.");
-				ChatHandler(player->GetSession()).PSendSysMessage(msg,
-					player->GetName());
-				return false;
-			}
-
 			
-			QueryResult coderesult = CharacterDatabase.PQuery("SELECT * FROM codes WHERE Code = %s", codes);
-			std::string codedb = (*coderesult)[0].GetString();
-			uint32 genutzt = (*coderesult)[3].GetUInt32();
-
-			if (codedb == ""){
-				char msg[250];
-				snprintf(msg, 250, "Dein Code wurde nicht akzeptiert.");
-				ChatHandler(player->GetSession()).PSendSysMessage(msg,
-					player->GetName());
-				return false;
-			}
-
-
-			if (genutzt == 0){
-				genutzt = 1;
-				CharacterDatabase.PExecute("UPDATE CODES set Spieler = %s, genutzt = %u WHERE Code = %s"
-					"(Spieler,genutzt) "
-					"VALUES ('%s', '%u')",
-					spieler, genutzt,codes);
-
 			
-				char msg[250];
-				snprintf(msg, 250, "Dein Code wurde akzeptiert. Deine Belohnung wurde dir gutgeschrieben.");
-				ChatHandler(player->GetSession()).PSendSysMessage(msg,
-					player->GetName());
+			switch (action)
+			{
+			case 1:
+			
+				
+					uint32 spieler = player->GetGUID();
+					std::string codes = code;
+
+					if (!*code){
+						char msg[250];
+						snprintf(msg, 250, "Es wurde kein Code eingetragen.");
+						ChatHandler(player->GetSession()).PSendSysMessage(msg,
+							player->GetName());
+						return false;
+					}
+
+
+					QueryResult coderesult = CharacterDatabase.PQuery("SELECT * FROM codes WHERE Code = %s", codes);
+					std::string codedb = (*coderesult)[0].GetString();
+					uint32 genutzt = (*coderesult)[3].GetUInt32();
+
+					if (codedb == ""){
+						char msg[250];
+						snprintf(msg, 250, "Dein Code wurde nicht akzeptiert.");
+						ChatHandler(player->GetSession()).PSendSysMessage(msg,
+							player->GetName());
+						return false;
+					}
+
+
+					if (genutzt == 0){
+						genutzt = 1;
+						CharacterDatabase.PExecute("UPDATE CODES set Spieler = %s, genutzt = %u WHERE Code = %s"
+							"(Spieler,genutzt) "
+							"VALUES ('%s', '%u')",
+							spieler, genutzt, codes);
+
+
+						char msg[250];
+						snprintf(msg, 250, "Dein Code wurde akzeptiert. Deine Belohnung wurde dir gutgeschrieben.");
+						ChatHandler(player->GetSession()).PSendSysMessage(msg,
+							player->GetName());
 
 
 
-				ss << "|cff54b5ffEin Code wurde eingeloest von |r " << ChatHandler(player->GetSession()).GetNameLink();
-				sWorld->SendGMText(LANG_GM_BROADCAST, ss.str().c_str());
+						ss << "|cff54b5ffEin Code wurde eingeloest von |r " << ChatHandler(player->GetSession()).GetNameLink();
+						sWorld->SendGMText(LANG_GM_BROADCAST, ss.str().c_str());
+						return true;
+		
+				}
+				
+				player->CLOSE_GOSSIP_MENU();
+
 				return true;
+			
+		}
+
+		return false;
+
+		
 
 			}
 		
