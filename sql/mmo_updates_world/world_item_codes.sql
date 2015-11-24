ebb55d72e9d87acadd90b97ae4401bf3eb6aba3d
Exitare <exi@pihaar.de>
2015-11-24 18:41:52 +0100

    [Custom] command gutschein

--- sql/mmo_updates_world/world_item_codes.sql
+++ sql/mmo_updates_world/world_item_codes.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `item_codes` (
   `code` INT NOT NULL COMMENT '',
   `belohnung` INT NULL COMMENT '',
-  `itemanzahl` INT NULL COMMENT '',
+  `anzahl` INT NULL COMMENT '',
   `benutzt` INT NULL COMMENT '',
   `name` VARCHAR(45) NULL COMMENT '',
-  `nutzbar` INT NULL COMMENT '',
   PRIMARY KEY (`code`)  COMMENT '');
