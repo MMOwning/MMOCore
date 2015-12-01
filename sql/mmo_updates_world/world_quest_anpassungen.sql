bb67d951ff8455a2360651f7f58a2b9b6d241942
Exitare <exi@pihaar.de>
2015-12-01 23:28:13 +0100

    [SQL] New file for NPC

--- sql/mmo_updates_world/world_quest_anpassungen.sql
+++ sql/mmo_updates_world/world_quest_anpassungen.sql
@@ -360,12 +360,6 @@ DELETE FROM gameobject WHERE `GUID` = 800475;
 UPDATE `creature_template` SET `ScriptName`='janarius' WHERE `entry`='800085';
 
 
-/* Firstausttattungs npc Umstellung */
-UPDATE `creature` SET `position_x`='-804.55', `position_y`='-1630.96', `position_z`='142.30', `orientation`='0.74' WHERE `guid`='800019';
-UPDATE `creature` SET `position_x`='-815.51', `position_y`='-1624.39', `position_z`='142.29', `orientation`='0.96' WHERE `guid`='800046';
-UPDATE `creature` SET `position_x`='-813.94', `position_y`='-1609.57', `position_z`='142.15', `orientation`='5.52' WHERE `guid`='800043';
-UPDATE `creature` SET `position_x`='-806.18', `position_y`='-1600.75', `position_z`='142.17', `orientation`='5.60' WHERE `guid`='800042';
-
 
 /* AStraler Kredit Goldkosten */
 UPDATE `item_template` SET `BuyPrice`='150000000' WHERE `entry`='38186';
---
+++ sql/mmo_updates_world/world_zfirstaustatter.sql
@@ -0,0 +1,4 @@
+UPDATE `creature` SET `position_x`='-804.55', `position_y`='-1630.96', `position_z`='142.30', `orientation`='0.74' WHERE `guid`='800019';
+UPDATE `creature` SET `position_x`='-815.51', `position_y`='-1624.39', `position_z`='142.29', `orientation`='0.96' WHERE `guid`='800046';
+UPDATE `creature` SET `position_x`='-813.94', `position_y`='-1609.57', `position_z`='142.15', `orientation`='5.52' WHERE `guid`='800043';
+UPDATE `creature` SET `position_x`='-806.18', `position_y`='-1600.75', `position_z`='142.17', `orientation`='5.60' WHERE `guid`='800042';
